5b88a43666193b398dc34a6fd416a6b0718c9a86
unknown <john.gordos@outlook.com>
2023-07-24 09:20:35 -0600

    updated index files, removing template comments and adding routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import AboutMe from "./about-me/about-me.js";
+import Footer from "./footer/footer.js";
+import Header from "./header/header.js";
+import Contact from "./contact/contact.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Router>
+    <Header />
+    <Routes>
+      <Route path="/" element={<AboutMe />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+    {/*
+    <Routes>
+      <Route path="/projects" element={<Projects />} />
+    </Routes>
+    */}
+    <Footer />
+  </Router>
+);
