5496f74a3e93be673887f75f311b1e1f111f897b
unknown <john.gordos@outlook.com>
2023-08-24 12:14:16 -0600

    added link to my trailblazer profile

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -6,6 +6,9 @@ function Footer() {
     <footer>
       <Link to={"https://github.com/ThePebble01"}>Github:ThePebble01</Link>
       <Link to={"mailto:john.gordos@outlook.com"}>john.gordos@outlook.com</Link>
+      <Link to={"https://www.salesforce.com/trailblazer/jgordos"}>
+        Trailblazer Profile
+      </Link>
     </footer>
   );
 }
