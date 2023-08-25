6a4561d31120dcd86d01148599ed209cc4133301
unknown <john.gordos@outlook.com>
2023-08-25 11:52:54 -0600

    reverting navigation after experimenting

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -1,23 +1,11 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Navigation() {
-  const navigate = useNavigate();
-  function handleClick(e) {
-    e.preventDefault();
-    const link = e.currentTarget.id;
-    navigate(`/${link}`);
-  }
   return (
-    <div className="tab">
-      <button id="aboutMe" className="tablinks" onClick={handleClick}>
-        About Me
-      </button>
-      <button id="projects" className="tablinks" onClick={handleClick}>
-        Projects
-      </button>
-      <button id="contact" className="tablinks" onClick={handleClick}>
-        Contact
-      </button>
-    </div>
+    <>
+      <Link to={"/"}>About Me</Link>
+      <Link to={"/projects"}>Projects</Link>
+      <Link to={"/contact"}>Contact</Link>
+    </>
   );
 }
