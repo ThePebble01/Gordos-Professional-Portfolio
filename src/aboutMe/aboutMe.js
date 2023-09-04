0cc26c69ce4560d3b5f9a0faea2864c39742c89b
unknown <john.gordos@outlook.com>
2023-09-04 15:46:18 -0600

    updated profile pic, about me, password generator image

--- src/aboutMe/aboutMe.js
+++ src/aboutMe/aboutMe.js
@@ -6,17 +6,18 @@ export default function App() {
     <section className="about-me">
       <h2>About Me</h2>
       <p>
-        John was raised in the City of Commerce City, Colorado and has found his
-        passion working with code to retrieve, transform, and display data for
-        users. Excellent problem-solving skills. Strong understanding of the
-        software development lifecycle on the Salesforce platform. Adept
-        knowledge of Salesforce’s Security Model allowing for proper user
-        management. Solid ability to utilize Apex, SOQL, Flows, Process
-        Builders. Solid understanding of Sales and Service Cloud features.
-        Expert at modeling data and customizing objects and fields. Recently
-        graduated from a full-stack coding boot camp through the University of
-        Denver to broaden his skills and facilitate his transition back to work.
-        Check out some of his work under the Project section!
+        John is a self-starter with a strong work ethic who values making things
+        easy and more efficient for users by applying technical expertise to
+        help maintain and implement scalable solutions. Excellent
+        problem-solving skills. Strong understanding of the software development
+        lifecycle on the Salesforce platform. Adept knowledge of Salesforce’s
+        Security Model allowing for proper user management. Solid ability to
+        utilize Apex, SOQL, Flows, Process Builders. Solid understanding of
+        Sales and Service Cloud features. Expert at modeling data and
+        customizing objects and fields. Recently graduated from a full-stack
+        coding boot camp through the University of Denver to broaden his skills
+        and facilitate his transition back to work. Check out some of his work
+        under the Project section!
       </p>
       <img src={devPicture} alt="The quote unquote developer." />
     </section>
